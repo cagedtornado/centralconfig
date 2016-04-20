acc5ee7c3f34a995dee4b40d1b6534c973acd810
Dan Esparza <esparza.dan@gmail.com>
2016-04-19 20:00:40 -0400

    Data: MySQL updates

--- datastores/mysql.go
+++ datastores/mysql.go
@@ -2,11 +2,13 @@ package datastores
 
 import (
 	"fmt"
+	"time"
 
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 )
 
+//	Requires at least MySQL 5.6 (for the auto updating datetime)
 var dbCreate = []byte(`
 CREATE TABLE configitem (
   id int(11) NOT NULL AUTO_INCREMENT,
@@ -53,21 +55,21 @@ func (store MySqlDB) Get(configItem *ConfigItem) (ConfigItem, error) {
 	retval := ConfigItem{}
 
 	//	Open the database:
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@%s(%s)/%s", store.User, store.Password, store.Protocol, store.Address, store.Database))
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@%s(%s)/%s?parseTime=true", store.User, store.Password, store.Protocol, store.Address, store.Database))
 	defer db.Close()
 	if err != nil {
 		return retval, err
 	}
 
-	//	Query to get default first
+	//	Prepare our query
 	stmt, err := db.Prepare("select id, application, name, value, machine, updated from configitem where application=? and name=? and machine=?")
 	if err != nil {
 		return retval, err
 	}
 	defer stmt.Close()
 
-	//	Query to get default first
-	rows, err := stmt.Query("*", configItem.Name, "")
+	//	Get the application/name/machine combo
+	rows, err := stmt.Query(configItem.Application, configItem.Name, configItem.Machine)
 	if err != nil {
 		return retval, err
 	}
@@ -78,20 +80,95 @@ func (store MySqlDB) Get(configItem *ConfigItem) (ConfigItem, error) {
 		var name string
 		var value string
 		var machine string
+		var updated time.Time
 
 		//	Scan the row into our variables
-		err = rows.Scan(&id, &application, &name, &value, &machine)
+		err = rows.Scan(&id, &application, &name, &value, &machine, &updated)
 
 		if err != nil {
 			return retval, err
 		}
 
 		//	Set our return value
-		retval.Id = id
-		retval.Application = application
-		retval.Name = name
-		retval.Value = value
-		retval.Machine = machine
+		retval = ConfigItem{
+			Id:          id,
+			Application: application,
+			Name:        name,
+			Value:       value,
+			Machine:     machine,
+			LastUpdated: updated}
+
+		break
+	}
+
+	//	If we haven't found it, get the application/name combo with a blank machine name
+	if retval.Id == 0 {
+		rows, err = stmt.Query(configItem.Application, configItem.Name, "")
+		if err != nil {
+			return retval, err
+		}
+
+		for rows.Next() {
+			var id int64
+			var application string
+			var name string
+			var value string
+			var machine string
+			var updated time.Time
+
+			//	Scan the row into our variables
+			err = rows.Scan(&id, &application, &name, &value, &machine, &updated)
+
+			if err != nil {
+				return retval, err
+			}
+
+			//	Set our return value
+			retval = ConfigItem{
+				Id:          id,
+				Application: application,
+				Name:        name,
+				Value:       value,
+				Machine:     machine,
+				LastUpdated: updated}
+
+			break
+		}
+	}
+
+	//	If we still haven't found it, get the default application/name and blank machine name
+	if retval.Id == 0 {
+		rows, err = stmt.Query("*", configItem.Name, "")
+		if err != nil {
+			return retval, err
+		}
+
+		for rows.Next() {
+			var id int64
+			var application string
+			var name string
+			var value string
+			var machine string
+			var updated time.Time
+
+			//	Scan the row into our variables
+			err = rows.Scan(&id, &application, &name, &value, &machine, &updated)
+
+			if err != nil {
+				return retval, err
+			}
+
+			//	Set our return value
+			retval = ConfigItem{
+				Id:          id,
+				Application: application,
+				Name:        name,
+				Value:       value,
+				Machine:     machine,
+				LastUpdated: updated}
+
+			break
+		}
 	}
 
 	return retval, nil
